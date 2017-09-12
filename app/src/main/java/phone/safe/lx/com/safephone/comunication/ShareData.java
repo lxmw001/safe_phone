7901dc475c6a21a55f760d6c174e642c386dc5a2
Luis Gonzalez <lgonzalez@ioet.com>
2017-09-12 11:10:24 -0500

    Refactoring sending location data code

--- app/src/main/java/phone/safe/lx/com/safephone/comunication/ShareData.java
+++ app/src/main/java/phone/safe/lx/com/safephone/comunication/ShareData.java
@@ -19,16 +19,11 @@ public class ShareData {
     public static void shareLocation(Location location, Context context) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         String macAddress = getMacAddress(context);
-        DatabaseReference latitudeRef = database.getReference("deviceData/" + macAddress + "/location/latitude");
-        DatabaseReference longitudeRef = database.getReference("deviceData/" + macAddress + "/location/longitude");
+        DatabaseReference currentLocationRef = database.getReference("deviceData/" + macAddress + "/location");
         DatabaseReference navigationRef = database.getReference("deviceData/" + macAddress + "/navigation/" + DateUtils.getCurrentDate());
-        DatabaseReference dateRef = database.getReference("deviceData/" + macAddress + "/location/date");
-        System.out.println("saving location on firebase");
-        latitudeRef.setValue(location.getLatitude());
-        longitudeRef.setValue(location.getLongitude());
-        dateRef.setValue(DateUtils.getCurrentDateAndTime());
-        navigationRef.push().setValue(getLocationMap(location));
 
+        currentLocationRef.setValue(getLocationMap(location));
+        navigationRef.push().setValue(getLocationMap(location));
     }	
 
     private static String getMacAddress(Context context) {
@@ -36,9 +31,10 @@ public class ShareData {
     }
 
     private static Map getLocationMap(Location location) {
-        Map<String, Double> locationMap = new HashMap<String, Double>();
+        Map<String, Object> locationMap = new HashMap<String, Object>();
         locationMap.put("latitude", location.getLatitude());
         locationMap.put("longitude", location.getLongitude());
+        locationMap.put("date", DateUtils.getCurrentDateAndTime());
         return locationMap;
     }
 }
