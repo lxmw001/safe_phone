567197cb616d9ad7e32d075705245d614c3c50db
Luis Gonzalez <lgonzalez@ioet.com>
2017-12-06 17:46:20 -0500

    Solving read mac address issue on android 6+

--- app/src/main/java/phone/safe/lx/com/safephone/utils/WifiUtils.java
+++ app/src/main/java/phone/safe/lx/com/safephone/utils/WifiUtils.java
@@ -4,12 +4,40 @@ import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
 public class WifiUtils {
 
     public static String getMacAddress(Context context) {
-        WifiManager manager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
-        WifiInfo info = manager.getConnectionInfo();
-        return info.getMacAddress();
+//        WifiManager manager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+//        WifiInfo info = manager.getConnectionInfo();
+//        return info.getMacAddress();
+
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:",b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+        }
+        return "02:00:00:00:00:00";
     }
     
 }
